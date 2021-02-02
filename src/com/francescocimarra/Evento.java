174af6866283a5e3e0dd35663df77720675fdb3c
cima-git01 <francesco.cimarra@gmail.com>
2021-02-02 08:44:06 +0100

    primo commit

---
+++ src/com/francescocimarra/Evento.java
@@ -0,0 +1,10 @@
+package com.francescocimarra;
+
+public class Evento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/francescocimarra/Protocollo.java
@@ -0,0 +1,10 @@
+package com.francescocimarra;
+
+public class Protocollo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
